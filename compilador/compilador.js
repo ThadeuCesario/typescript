99724439c1d7261b5936f3ba56fc21ab9e06bfaa
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2015-02-02 12:50:39 -0200

    testing noEmitOnError prop in tsconfig

---
+++ compilador/compilador.js
@@ -0,0 +1,5 @@
+"use strict";
+let canalDois = 'Gaveta';
+let inscritosDois = 12123;
+// canal = inscritos;
+console.log(canalDois);
---
+++ compilador/compilador.ts
@@ -0,0 +1,5 @@
+let canalDois: string = 'Gaveta';
+let inscritosDois: number = 12123;
+
+// canal = inscritos;
+console.log(canalDois);
