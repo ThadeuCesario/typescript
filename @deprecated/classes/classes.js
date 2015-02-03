5e544b5c0ee4ea266127cfd3d7d8f700776f0215
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2015-02-03 13:17:16 -0200

    testing sourceMap prop in ts config

--- @deprecated/classes/classes.js
+++ @deprecated/classes/classes.js
@@ -319,3 +319,4 @@ const turboHelice = new Aviao('Tu-114', 'PT-ABC');
 //turboHelice.modelo = 'DC-8';
 //turboHelice.prefixo = 'Ab-123';
 console.log(turboHelice);
+//# sourceMappingURL=classes.js.map
