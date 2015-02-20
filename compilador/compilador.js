0f6d44397fc5b900fd3e64db138f640013969e2d
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2015-02-20 14:20:56 -0200

    testing some options in tsconfig

--- compilador/compilador.js
+++ compilador/compilador.js
@@ -3,4 +3,24 @@ let canalDois = 'Gaveta';
 let inscritosDois = 12123;
 // canal = inscritos;
 console.log(canalDois);
+// function saudar(isManha: boolean, horas: number): string { //noUnusedParameters
+//     let a = 1; // noUnusedLocals
+//     let saudacao: string;
+//     if(isManha) {
+//         saudacao = 'Bom dia';
+//     } else {
+//         saudacao = 'Aproveite o drink'
+//     }
+//     return saudacao; //strictNullChecks
+// }
+function saudarNew(isManha) {
+    let saudacao;
+    if (isManha) {
+        saudacao = 'Bom dia';
+    }
+    else {
+        saudacao = 'Aproveite o drink';
+    }
+    return saudacao;
+}
 //# sourceMappingURL=compilador.js.map
