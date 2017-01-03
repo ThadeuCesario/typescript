71c8dc54f3b9569d30ee42e7dbb543e6c98d262e
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2017-01-03 14:44:40 -0200

    checking let and const transpile in typescript

---
+++ ecmascript/ecmascript.ts
@@ -0,0 +1,20 @@
+// let & const
+// console.log(seraQuePode)
+// var seraQuePode = '?'; //hoisting
+
+let seraQuePode = `?`;
+console.log(seraQuePode);
+
+let estaFrioNew = true;
+
+if (estaFrioNew) {
+    let acao = 'colocar o casaco'
+    console.log(acao);
+}
+
+
+const cpfUser: string = '111.222.333-99';
+// cpf = '111.222.333-00';
+console.log(cpf);
+
+// const e let possuem escopo de função, bloco e global
