5caccbac4f35a8ddd969df369128906fd548e9ce
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-24 22:40:02 -0300

    tuplas - typescript

--- types/types.js
+++ types/types.js
@@ -24,3 +24,25 @@ myAge = 27;
 console.log(typeof myAge); //number
 myAge = 'idade é 27';
 console.log(typeof myAge); //string
+/**
+ * Tipo array
+ * Quando criamos um array, o valor que é contido dentro do array também é inferido. Portanto, no caso abaixo temos um array de string.
+ * Quando eu tento atribuir um tipo número, o typescript apontará que não é possível.
+ *
+ * Para resolvermos esse problema, podemos declarar a tipagem da seguinte forma:
+ * let hobbies: any[] = ['Estudar javascript', 'Cubo Mágico', 'Fazer sudoku', 'Ver vídeos'];
+ *
+ * Veja que temos um 'any[]', significa que estamos trabalhando com um array que suporta qualquer tipo de elementos.
+ */
+var hobbies = ['Estudar javascript', 'Cubo Mágico', 'Fazer sudoku', 'Ver vídeos'];
+console.log(hobbies);
+console.log(typeof hobbies); // retorna um Object
+hobbies = [100];
+console.log(hobbies);
+/**
+ * Outra funcionalidade do typescript são as Tuplas.
+ * Basicamente definimos um array que podemos declarar seus tipos de forma ordenada. Então por exemplo: "Quero um array que o primeiro elemento seja um string,
+ * o segundo um number e o terceiro um number";
+ * Podemos fazer isso, utilizando tuplas.
+ *
+ */ 
