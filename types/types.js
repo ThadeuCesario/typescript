4087d1a941f9790d5d1b40890600965bbcf421c9
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-24 22:26:31 -0300

    tipos dinamicos

--- types/types.js
+++ types/types.js
@@ -8,3 +8,17 @@ var userAge = 27;
 //userAge = 'Ana';
 userAge = 27.5; //O javascript e typescript não difere numero inteiro de numero flutuante.
 console.log(userAge);
+//boolean
+var hasHobbies = false;
+//hasHobbies = 1;
+console.log(hasHobbies);
+/**
+ * Quando trabalhamos com tipos explícitos. Criamos uma variável dinâmica que é perfeitamente interpretada pelo Typescript.
+ * Então, veja no caso abaixo que primeiramente tinhamos uma idade que foi atribuido um tipo number,
+ * em seguida atribuímos o tipo string.
+ */
+var myAge;
+myAge = 27;
+console.log(typeof myAge); //number
+myAge = 'idade é 27';
+console.log(typeof myAge); //string
