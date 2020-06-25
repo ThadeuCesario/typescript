6cbc39cef2796d63ff4284169c31a72b76b2363c
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-24 22:27:34 -0300

    tipos dinamicos

--- types/types.ts
+++ types/types.ts
@@ -16,7 +16,7 @@ console.log(hasHobbies);
 
 
 /**
- * Quando trabalhamos com tipos explícitos. Criamos uma variável dinâmica que é perfeitamente interpretada pelo Typescript.
+ * Quando trabalhamos sem nenhum tipo explícito, criamos uma variável dinâmica que é perfeitamente interpretada pelo Typescript.
  * Então, veja no caso abaixo que primeiramente tinhamos uma idade que foi atribuido um tipo number,
  * em seguida atribuímos o tipo string.
  */
