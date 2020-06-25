75475691f410c55ca6f66238a3d6619d92d18250
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-24 22:21:04 -0300

    boolean type

--- types/types.js
+++ types/types.js
@@ -3,3 +3,8 @@
 var userName = 'Thadeu';
 console.log(userName);
 //userName = 28;
+//numbers
+var userAge = 27;
+//userAge = 'Ana';
+userAge = 27.5; //O javascript e typescript não difere numero inteiro de numero flutuante.
+console.log(userAge);
--- types/types.ts
+++ types/types.ts
@@ -2,3 +2,9 @@
 let userName = 'Thadeu';
 console.log(userName);
 //userName = 28;
+
+//numbers
+let userAge = 27;
+//userAge = 'Ana';
+userAge = 27.5; //O javascript e typescript não difere numero inteiro de numero flutuante.
+console.log(userAge);
