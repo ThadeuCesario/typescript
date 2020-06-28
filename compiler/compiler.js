4fcc34a9fff6b531a28767aed11f93019d886d4d
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-28 13:53:52 -0300

    noEmitOnError: true   - typescript settings

--- compiler/compiler.js
+++ compiler/compiler.js
@@ -2,4 +2,11 @@
 /**
  * Se quiseremos podemos configurar nosso typescript para gerar o arquivo javascript mesmo que tenha algum erro apontado pelo typescript.
  * Porém, podemos configurar para que não seja gerado o javascript caso exista algum erro no typescript.
- */ 
+ * Para isso, basta no tsconfig.json configurarmos a propriedade:
+ * "noEmitOnError": true,
+ * Assim, quando algum erro de typescript for detectado, não será gerado o arquivo javascript.
+ */
+var canal = 'Gaveta';
+var inscritos = 6210234;
+//canal = inscritos;
+console.log("Canal = " + canal);
--- compiler/compiler.ts
+++ compiler/compiler.ts
@@ -1,4 +1,13 @@
   /**
    * Se quiseremos podemos configurar nosso typescript para gerar o arquivo javascript mesmo que tenha algum erro apontado pelo typescript.
    * Porém, podemos configurar para que não seja gerado o javascript caso exista algum erro no typescript.
+   * Para isso, basta no tsconfig.json configurarmos a propriedade:
+   * "noEmitOnError": true,
+   * Assim, quando algum erro de typescript for detectado, não será gerado o arquivo javascript.
    */
+
+   let canal: string = 'Gaveta';
+   let inscritos: number = 6210234;
+
+   //canal = inscritos;
+   console.log(`Canal = ${canal}`);
