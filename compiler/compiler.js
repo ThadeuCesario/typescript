cbcedf3741160503ee593629da7594dc8f88ca31
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-28 13:38:40 -0300

    compiler typescript

---
+++ compiler/compiler.js
@@ -0,0 +1,5 @@
+"use strict";
+/**
+ * Se quiseremos podemos configurar nosso typescript para gerar o arquivo javascript mesmo que tenha algum erro apontado pelo typescript.
+ * Porém, podemos configurar para que não seja gerado o javascript caso exista algum erro no typescript.
+ */ 
