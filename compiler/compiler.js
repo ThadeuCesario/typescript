7bdd14ccc4e50734c29b507901332199f77b6e15
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-28 14:30:09 -0300

    target - settings typescript

--- compiler/compiler.js
+++ compiler/compiler.js
@@ -1,10 +1,18 @@
 "use strict";
 /**
+ * Configurações:
+ * (As configurações são realizadas dentro do arquivo tsconfig.json)
+ *
+ * noEmitOnError
  * Se quiseremos podemos configurar nosso typescript para gerar o arquivo javascript mesmo que tenha algum erro apontado pelo typescript.
  * Porém, podemos configurar para que não seja gerado o javascript caso exista algum erro no typescript.
  * Para isso, basta no tsconfig.json configurarmos a propriedade:
  * "noEmitOnError": true,
  * Assim, quando algum erro de typescript for detectado, não será gerado o arquivo javascript.
+ *
+ * Target
+ * Essa configuração define, qual será a versão do javascript que será utilizada no arquivo compilado. Por padrão o target vemm configurado no ES5.
+ *
  */
 var canal = 'Gaveta';
 var inscritos = 6210234;
--- compiler/compiler.ts
+++ compiler/compiler.ts
@@ -1,9 +1,17 @@
   /**
+   * Configurações:
+   * (As configurações são realizadas dentro do arquivo tsconfig.json)
+   * 
+   * noEmitOnError
    * Se quiseremos podemos configurar nosso typescript para gerar o arquivo javascript mesmo que tenha algum erro apontado pelo typescript.
    * Porém, podemos configurar para que não seja gerado o javascript caso exista algum erro no typescript.
    * Para isso, basta no tsconfig.json configurarmos a propriedade:
    * "noEmitOnError": true,
    * Assim, quando algum erro de typescript for detectado, não será gerado o arquivo javascript.
+   * 
+   * Target
+   * Essa configuração define, qual será a versão do javascript que será utilizada no arquivo compilado. Por padrão o target vemm configurado no ES5.
+   * 
    */
 
    let canal: string = 'Gaveta';
