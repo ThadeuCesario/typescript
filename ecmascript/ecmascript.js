e1b25f4c7ded51535fcda7818b431ff5f8f14391
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2022-02-24 17:33:01 -0300

    more studies about variables in javascript...

--- ecmascript/ecmascript.js
+++ ecmascript/ecmascript.js
@@ -3,7 +3,7 @@ let seraQuePode = `?`;
 console.log(seraQuePode);
 // let seraQuePode = `?`; //hoisting
 // var -> escopo de função e global 
-// let -> escopo de função, global e bloco
+// let e const -> escopo de função, global e bloco
 let estaFrio = true;
 if (estaFrio) {
     let acao = 'Colocar o casaco!';
@@ -12,4 +12,19 @@ if (estaFrio) {
 const cpf = '123.456.000-99';
 // cpf='456.780.999.11';
 console.log(cpf);
+var segredo = 'externo';
+function revelar() {
+    var segredo = 'interno';
+    console.log(segredo);
+}
+revelar();
+console.log(segredo);
+{
+    const def = `def`;
+    console.log(def);
+}
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log(i);
 //# sourceMappingURL=ecmascript.js.map
--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -4,7 +4,7 @@ console.log(seraQuePode);
 
 
 // var -> escopo de função e global 
-// let -> escopo de função, global e bloco
+// let e const -> escopo de função, global e bloco
 let estaFrio = true;
 if(estaFrio) {
     let acao = 'Colocar o casaco!';
@@ -14,3 +14,21 @@ if(estaFrio) {
 const cpf:string = '123.456.000-99';
 // cpf='456.780.999.11';
 console.log(cpf);
+
+var segredo = 'externo';
+function revelar() {
+    var segredo = 'interno';
+    console.log(segredo);
+}
+revelar();
+console.log(segredo);
+
+{
+    const def=`def`;
+    console.log(def);
+}
+
+for(var i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log(i);
