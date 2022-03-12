6f2a95d61d33eddb09c9aadb2e1486b457d03bbb
ThadeuCesario <thadeu.cesario@icloud.com>
2022-03-12 11:57:44 -0300

    using interfaces with class

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -3,15 +3,34 @@ function saudarComOla(pessoa) {
     console.log(`Olá ${pessoa.nome}`);
 }
 function mudarNome(pessoa) {
-    pessoa.nome = 'Karina';
+    // pessoa.nome = 'Karina';
 }
 const pessoa = {
-    nome: 'Thadeu Munhóz Cesário',
-    idade: 27
+    nome: 'Eric',
+    idade: 27,
+    saudar(sobrenome) {
+        console.log(`Olá, meu nome é ${this.nome} ${sobrenome}`);
+    }
 };
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
-saudarComOla({ nome: 'Eric Cartman', idade: 27 });
-saudarComOla({ nome: 'Kyle' });
+// saudarComOla({nome: 'Eric Cartman', idade: 27, altura: 1.75});
+// saudarComOla({nome: 'Kyle'});
+pessoa.saudar('Cartman');
+// Usando classes...
+class Cliente {
+    constructor() {
+        this.nome = '';
+        this.ultimaCompra = new Date;
+    }
+    saudar(sobrenome) {
+        console.log(`Olá, meu nome é ${this.nome} ${sobrenome}`);
+    }
+}
+const meuCliente = new Cliente();
+meuCliente.nome = 'Han';
+saudarComOla(meuCliente);
+meuCliente.saudar('Solo');
+console.log(meuCliente.ultimaCompra);
 //# sourceMappingURL=interfaces.js.map
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,6 +1,8 @@
 interface Humano {
     nome: string;
     idade?: number;
+    [prop: string]: any; // Propriedades que o atributo pode variar
+    saudar(sobrenome: string): void;
 }
 
 function saudarComOla(pessoa: Humano) {
@@ -8,16 +10,37 @@ function saudarComOla(pessoa: Humano) {
 }
 
 function mudarNome(pessoa: Humano) {
-    pessoa.nome = 'Karina'
+    // pessoa.nome = 'Karina';
 }
 
-const pessoa = {
-    nome: 'Thadeu Munhóz Cesário',
-    idade: 27
+const pessoa: Humano = {
+    nome: 'Eric',
+    idade: 27,
+    saudar(sobrenome: string): void {
+        console.log(`Olá, meu nome é ${this.nome} ${sobrenome}`)
+    }
 };
 
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
-saudarComOla({nome: 'Eric Cartman', idade: 27});
-saudarComOla({nome: 'Kyle'});
+// saudarComOla({nome: 'Eric Cartman', idade: 27, altura: 1.75});
+// saudarComOla({nome: 'Kyle'});
+pessoa.saudar('Cartman');
+
+
+// Usando classes...
+
+class Cliente implements Humano {
+    nome: string = '';
+    ultimaCompra: Date = new Date;
+    saudar(sobrenome: string): void {
+        console.log(`Olá, meu nome é ${this.nome} ${sobrenome}`)
+    }
+}
+
+const meuCliente = new Cliente();
+meuCliente.nome = 'Han';
+saudarComOla(meuCliente);
+meuCliente.saudar('Solo');
+console.log(meuCliente.ultimaCompra);
