1f9d650c70e6c22a3ae95a2dd3a06d581a507356
ThadeuCesario <thadeu.cesario@icloud.com>
2022-03-14 15:03:41 -0300

    interfaces in typescript

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -3,7 +3,7 @@ function saudarComOla(pessoa) {
     console.log(`Olá ${pessoa.nome}`);
 }
 function mudarNome(pessoa) {
-    // pessoa.nome = 'Karina';
+    pessoa.nome = 'Karina';
 }
 const pessoa = {
     nome: 'Eric',
@@ -37,4 +37,36 @@ let potencia;
 potencia = function (base, exp) {
     return Math.pow(base, exp);
 };
+class RealA {
+    a() { }
+}
+class RealAB {
+    a() { }
+    ;
+    b() { }
+    ;
+}
+class RealABC {
+    a() { }
+    ;
+    b() { }
+    ;
+    c() { }
+    ;
+}
+class AbstrataABD {
+    a() { }
+    ;
+    b() { }
+    ;
+}
+Object.prototype.log = function () {
+    console.log(this.toString());
+};
+const x = 2;
+const y = 3;
+const z = 4;
+x.log();
+y.log();
+z.log();
 //# sourceMappingURL=interfaces.js.map
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -10,7 +10,7 @@ function saudarComOla(pessoa: Humano) {
 }
 
 function mudarNome(pessoa: Humano) {
-    // pessoa.nome = 'Karina';
+    pessoa.nome = 'Karina';
 }
 
 const pessoa: Humano = {
@@ -54,3 +54,54 @@ let potencia: FuncaoCalculo;
 potencia = function(base: number, exp: number): number {
     return Math.pow(base, exp);
 }
+
+// Herança
+
+interface A {
+    a(): void;
+}
+
+interface B {
+    b(): void;
+}
+
+interface ABC extends A, B{
+    c(): void;
+}
+
+class RealA implements A {
+    a(): void {}
+}
+
+class RealAB implements A, B {
+    a(): void {};
+    b(): void {};
+}
+
+class RealABC implements ABC {
+    a(): void {};
+    b(): void {};
+    c(): void {};
+}
+
+abstract class AbstrataABD implements A, B {
+    a(): void {};
+    b(): void {};
+    abstract d(): void;
+}
+
+interface Object {
+    log(): void
+}
+
+Object.prototype.log = function() {
+    console.log(this.toString());
+}
+
+const x = 2;
+const y = 3;
+const z = 4;
+
+x.log();
+y.log();
+z.log();
