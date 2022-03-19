8d918e2714dca8adcbfe47eadb86a7ee1efdeb34
ThadeuCesario <thadeu.cesario@icloud.com>
2022-03-19 14:09:13 -0300

    using generics with class - more examples

--- genericos/genericos.js
+++ genericos/genericos.js
@@ -52,4 +52,20 @@ class Somabinaria extends OperacaoBinaria {
     }
 }
 console.log(new Somabinaria(3, 4).executar());
+class DiferencaEntreDatas extends OperacaoBinaria {
+    getTime(data) {
+        let { dia, mes, ano } = data;
+        return new Date(`${mes}/${dia}/${ano}`).getTime();
+    }
+    executar() {
+        const t1 = this.getTime(this.operando1);
+        const t2 = this.getTime(this.operando2);
+        const diferenca = Math.abs(t1 - t2);
+        const dia = 1000 * 60 * 60 * 24;
+        return `${Math.ceil(diferenca / dia)} dia(s)`;
+    }
+}
+const d1 = new Data(1, 2, 2020);
+const d2 = new Data(5, 2, 2020);
+console.log(new DiferencaEntreDatas(d1, d2).executar());
 //# sourceMappingURL=genericos.js.map
