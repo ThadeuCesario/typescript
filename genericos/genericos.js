288b824505edde4ddb789f37a2389744689edd5d
ThadeuCesario <thadeu.cesario@icloud.com>
2022-03-22 16:09:06 -0300

    last challenge - generics chapter

--- genericos/genericos.js
+++ genericos/genericos.js
@@ -119,4 +119,35 @@ console.log(filaDaCoder.proximo());
 console.log(filaDaCoder.proximo());
 console.log(filaDaCoder.proximo());
 filaDaCoder.imprimir();
+class Mapa {
+    constructor() {
+        this.itens = new Array();
+    }
+    obter(chave) {
+        const resultado = this.itens.filter(i => i.chave === chave);
+        return resultado ? resultado[0] : null;
+    }
+    colocar(par) {
+        const encontrado = this.obter(par.chave);
+        if (encontrado)
+            encontrado.valor = par.valor;
+        else
+            this.itens.push(par);
+    }
+    limpar() {
+        this.itens = new Array();
+    }
+    imprimir() {
+        console.log(this.itens);
+    }
+}
+const mapa = new Mapa();
+mapa.colocar({ chave: 1, valor: 'Pedro' });
+mapa.colocar({ chave: 2, valor: 'Rebeca' });
+mapa.colocar({ chave: 3, valor: 'Maria' });
+mapa.colocar({ chave: 1, valor: 'Gustavo' });
+console.log(mapa.obter(2));
+mapa.imprimir();
+mapa.limpar();
+mapa.imprimir();
 //# sourceMappingURL=genericos.js.map
