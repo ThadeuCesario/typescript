7c1901c28b04e2692dd151df2de1a14a7f4987dd
ThadeuCesario <thadeu.cesario@icloud.com>
2022-03-23 15:11:40 -0300

    changing constructor with class decorators

--- decorators/decorators.js
+++ decorators/decorators.js
@@ -5,16 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-// @logarClasse //Esse decorator será chamado somente quando a classe for carregada
-// @logarClasseSe(true)
-let Eletrodomestico = class Eletrodomestico {
-    constructor() {
-        console.log("novo.....");
-    }
-};
-Eletrodomestico = __decorate([
-    decorator({ a: 'Teste', b: 123 })
-], Eletrodomestico);
 function logarClasse(construtor) {
     console.log(construtor);
 }
@@ -27,4 +17,28 @@ function decorator(obj) {
         console.log(obj.a + ' ' + obj.b);
     };
 }
+// @logarClasse //Esse decorator será chamado somente quando a classe for carregada
+// @decorator({a: 'Teste', b: 123})
+// @logarClasseSe(true)
+let Eletrodomestico = class Eletrodomestico {
+    constructor() {
+        console.log("novo.....");
+    }
+};
+Eletrodomestico = __decorate([
+    logarObjeto
+], Eletrodomestico);
+function logarObjeto(construtor) {
+    console.log("Carregado...");
+    return class extends construtor {
+        constructor(...args) {
+            console.log("Antes....");
+            super(...args);
+            console.log("Depois....");
+        }
+    };
+}
+new Eletrodomestico();
+new Eletrodomestico();
+new Eletrodomestico();
 //# sourceMappingURL=decorators.js.map
--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,12 +1,3 @@
-// @logarClasse //Esse decorator será chamado somente quando a classe for carregada
-// @logarClasseSe(true)
-@decorator({a: 'Teste', b: 123})
-class Eletrodomestico {
-    constructor() {
-        console.log("novo.....")
-    }
-}
-
 function logarClasse(construtor: Function) {
     console.log(construtor);
 }
@@ -17,8 +8,35 @@ function logarClasseSe(valor: boolean) {
     return valor ? logarClasse : decoratorVazio;
 }
 
-function decorator(obj: { a: string, b: number }) {
+function decorator(obj: { a: string, b?: number }) {
     return function(_: Function): void {
         console.log(obj.a + ' ' + obj.b);
     }
 }
+
+// @logarClasse //Esse decorator será chamado somente quando a classe for carregada
+// @decorator({a: 'Teste', b: 123})
+// @logarClasseSe(true)
+@logarObjeto
+class Eletrodomestico {
+    constructor() {
+        console.log("novo.....")
+    }
+}
+
+type Construtor = {new(...args: any[]): {}} // Construtor mais genérico possível
+
+function logarObjeto(construtor: Construtor) {
+    console.log("Carregado...");
+    return class extends construtor {
+        constructor(...args: any[]) {
+            console.log("Antes....");
+            super(...args);
+            console.log("Depois....");
+        }
+    }
+}
+
+new Eletrodomestico();
+new Eletrodomestico();
+new Eletrodomestico();
