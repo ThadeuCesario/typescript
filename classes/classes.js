3876a1be6915c1c4c377d3123e3e878ca1352ade
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2022-06-06 20:49:33 -0300

    new way to create public atributes

--- classes/classes.js
+++ classes/classes.js
@@ -14,4 +14,19 @@ console.log(aniversario);
 const casamento = new Data; // posso omitir os ()
 casamento.ano = 2017;
 console.log(casamento);
+//========================================
+class DataEsperta {
+    constructor(dia = 1, mes = 1, ano = 1970) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
+const aniversarioEsperto = new DataEsperta(4, 11, 1994);
+aniversarioEsperto.dia = 4;
+console.log(aniversarioEsperto.dia);
+console.log(aniversarioEsperto);
+const casamentoEsperto = new DataEsperta; // posso omitir os ()
+casamentoEsperto.ano = 2017;
+console.log(casamentoEsperto);
 //# sourceMappingURL=classes.js.map
--- classes/classes.ts
+++ classes/classes.ts
@@ -19,3 +19,21 @@ console.log(aniversario);
 const casamento = new Data // posso omitir os ()
 casamento.ano = 2017;
 console.log(casamento)
+//========================================
+
+class DataEsperta {
+    constructor(
+        public dia:number = 1, 
+        public mes:number = 1, 
+        public ano:number = 1970
+    ) {}
+}
+
+const aniversarioEsperto = new DataEsperta(4, 11, 1994);
+aniversarioEsperto.dia = 4;
+console.log(aniversarioEsperto.dia);
+console.log(aniversarioEsperto);
+
+const casamentoEsperto = new DataEsperta // posso omitir os ()
+casamentoEsperto.ano = 2017;
+console.log(casamentoEsperto)
