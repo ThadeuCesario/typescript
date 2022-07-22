1ff46af8cbd8482de76e6e40166fbd99c286607f
ThadeuCesario <thadeu.cesario@icloud.com>
2022-07-22 11:28:45 -0300

    namespaces in multiples files

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,21 +1,37 @@
-"use strict";
 var Geometria;
 (function (Geometria) {
-    let Area;
+    var Area;
     (function (Area) {
-        const PI = 3.14;
+        var PI = 3.14;
         function circunferencia(raio) {
             return PI * Math.pow(raio, 2);
         }
         Area.circunferencia = circunferencia;
+    })(Area = Geometria.Area || (Geometria.Area = {}));
+})(Geometria || (Geometria = {}));
+var Geometria;
+(function (Geometria) {
+    var Area;
+    (function (Area) {
+        var PI = 3.14;
         function retangulo(base, altura) {
             return base * altura;
         }
         Area.retangulo = retangulo;
     })(Area = Geometria.Area || (Geometria.Area = {}));
 })(Geometria || (Geometria = {}));
-const PI = 2.99;
+// namespace Geometria {
+//     export namespace Area {
+//         const PI = 3.14;
+//         export function circunferencia(raio: number): number {
+//             return PI * Math.pow(raio, 2);
+//         }
+//         export function retangulo(base: number, altura: number): number {
+//             return base * altura;
+//         }
+//     }
+// }
+var PI = 2.99;
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
 console.log(PI);
-//# sourceMappingURL=namespaces.js.map
--- namespaces/namespaces.ts
+++ namespaces/namespaces.ts
@@ -1,16 +1,16 @@
-namespace Geometria {
-    export namespace Area {
-        const PI = 3.14;
+// namespace Geometria {
+//     export namespace Area {
+//         const PI = 3.14;
         
-        export function circunferencia(raio: number): number {
-            return PI * Math.pow(raio, 2);
-        }
+//         export function circunferencia(raio: number): number {
+//             return PI * Math.pow(raio, 2);
+//         }
         
-        export function retangulo(base: number, altura: number): number {
-            return base * altura;
-        }
-    }
-}
+//         export function retangulo(base: number, altura: number): number {
+//             return base * altura;
+//         }
+//     }
+// }
 
 const PI = 2.99
 
