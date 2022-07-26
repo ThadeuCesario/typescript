76b4d7b7203b6fba7887da1005796313dc96fc93
ThadeuCesario <thadeu.cesario@icloud.com>
2022-07-26 08:46:34 -0300

    last class about modules and namespaces in typescript

--- modulos/modulos.js
+++ modulos/modulos.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const retangulo_1 = require("./retangulo");
+// import { areaCircunferencia } from './circunferencia';
 const circunferencia_1 = require("./circunferencia");
+console.log('modulo carregado...');
 console.log((0, retangulo_1.areaRetangulo)(7, 8));
 console.log((0, circunferencia_1.areaCircunferencia)(2));
+const { digaOi } = require('./novo');
+console.log(digaOi('Thadeu'));
 //# sourceMappingURL=modulos.js.map
--- modulos/modulos.ts
+++ modulos/modulos.ts
@@ -1,5 +1,10 @@
 import { areaRetangulo } from './retangulo';
-import { areaCircunferencia } from './circunferencia';
+// import { areaCircunferencia } from './circunferencia';
+import { areaCircunferencia as circ} from './circunferencia';
 
+console.log('modulo carregado...')
 console.log(areaRetangulo(7, 8));
-console.log(areaCircunferencia(2));
+console.log(circ(2));
+
+const {digaOi} = require('./novo'); 
+console.log(digaOi('Thadeu'))
