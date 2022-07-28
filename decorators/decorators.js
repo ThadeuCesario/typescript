4d9ad865f0c057e3d38da4fb2ccf8cf3b256b28b
ThadeuCesario <thadeu.cesario@icloud.com>
2022-07-28 14:56:29 -0300

    decorator factory in typescript

--- decorators/decorators.js
+++ decorators/decorators.js
@@ -5,15 +5,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+// @logarClasse //Esse decorator será chamado somente quando a classe for carregada
+// @logarClasseSe(true)
 let Eletrodomestico = class Eletrodomestico {
     constructor() {
         console.log("novo.....");
     }
 };
 Eletrodomestico = __decorate([
-    logarClasse //Esse decorator será chamado somente quando a classe for carregada
+    decorator({ a: 'Teste', b: 123 })
 ], Eletrodomestico);
 function logarClasse(construtor) {
     console.log(construtor);
+}
+function decoratorVazio(_) { }
+function logarClasseSe(valor) {
+    return valor ? logarClasse : decoratorVazio;
+}
+function decorator(obj) {
+    return function (_) {
+        console.log(obj.a + ' ' + obj.b);
+    };
 }
 //# sourceMappingURL=decorators.js.map
--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,11 +1,24 @@
-@logarClasse //Esse decorator será chamado somente quando a classe for carregada
+// @logarClasse //Esse decorator será chamado somente quando a classe for carregada
+// @logarClasseSe(true)
+@decorator({a: 'Teste', b: 123})
 class Eletrodomestico {
     constructor() {
         console.log("novo.....")
     }
 }
 
-
-function logarClasse(construtor: Function){
+function logarClasse(construtor: Function) {
     console.log(construtor);
+}
+
+function decoratorVazio(_: Function) {}
+
+function logarClasseSe(valor: boolean) {
+    return valor ? logarClasse : decoratorVazio;
+}
+
+function decorator(obj: { a: string, b: number }) {
+    return function(_: Function): void {
+        console.log(obj.a + ' ' + obj.b);
+    }
 }
